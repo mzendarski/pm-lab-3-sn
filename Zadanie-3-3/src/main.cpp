dc332bdf9f8c446b9ba566689ea2b6f390590425
Mikolaj <mikolaj.zendarski@student.wat.edu.pl>
2020-12-14 11:36:39 +0100

    Wykonano zadanie 3.3.2 – ADCsingleREAD

--- Zadanie-3-3/src/main.cpp
+++ Zadanie-3-3/src/main.cpp
@@ -1,18 +1,33 @@
 #include <Arduino.h>
-
 #define ADC_RESOLUTION_DEFAULT (5.0f / 1024.0f)
 
 uint16_t digital;
 float voltage;
-void setup() {
+
+uint16_t ADCsingleREAD(uint8_t adcToUse){
+  uint16_t ADCval;
+
+  ADMUX = adcToUse;                                    //Konfiguracja numeru wejścia
+  ADMUX |= (1 << REFS0);                               //Konfiguracja napięcia odniesienia - zewnętrzne 5v
+  ADMUX &= ~(1 << ADLAR);                              //Rozdzielczość 10 bitów
+  ADCSRA|= (1 << ADPS2) | (1 << ADPS1) | (1 >> ADPS0); //Konfiguracja presklara na 125Khz
+  ADCSRA |= (1 << ADEN);                               //Włączeniue ADC
+  ADCSRA |= (1 <<ADSC);                                //Uruchomienie konwersji
+  while (ADCSRA & (1 << ADSC))
+    ;                           //Oczekiwanie na zakończenie konwersji
+  ADCval = ADCL;                //Odczyt młodszych bitów wyniku konwersji
+  ADCval = (ADCH << 8) +ADCval; //odczyt starszych bitów wyniku kowersji
+  
+  return ADCval;                //Zwrócenie wyniku konwersji
+}
+
+void setup(){
   Serial.begin(9600);
- analogReference(DEFAULT);
- pinMode(A0, INPUT);
 }
 
 void loop(){
-  digital = analogRead(A0);
-  voltage = ADC_RESOLUTION_DEFAULT * digital;
+  digital = ADCsingleREAD(0);
+  voltage =ADC_RESOLUTION_DEFAULT * digital;
   Serial.print("A0 = ");
   Serial.print(digital);
   Serial.print(" \t V(0) = ");
